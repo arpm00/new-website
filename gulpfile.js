27ae8a86e7f889966e893e314d072e549f8a0949
arpm00 <arpm00@gmail.com>
2019-10-01 23:44:15 +0400

    CSS organization

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,14 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssvars = require('postcss-simple-vars');
 const nested = require('postcss-nested');
+const cssimport = require('postcss-import');
 
 //compile scss to css
 function style() {
   //1.where is my scss file
   return gulp.src('./app/assets/styles/styles.css')
   //2.pass the file through scss compiler
-  .pipe(postcss([cssvars, nested, autoprefixer]))//.on('error', sass.logError))
+  .pipe(postcss([cssimport, cssvars, nested, autoprefixer]))//.on('error', sass.logError))
   //3.where do i save the compiled css
   .pipe(gulp.dest('./app/temp/styles'));
   //4.stream changes to all browser
