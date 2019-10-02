24ed4c5570e9a4fe6ca23ba4b85da0fd496053eb
arpm00 <arpm00@gmail.com>
2019-10-02 23:54:26 +0400

    Modified gulpfile and continue building the website

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,39 @@
 const gulp = require('gulp');
 //const sass = require('gulp-sass');
-//const browserSync = require('browser-sync').create();
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssvars = require('postcss-simple-vars');
 const nested = require('postcss-nested');
 const cssimport = require('postcss-import');
+const browserSync = require('browser-sync').create();
+const mixins = require('postcss-mixins');
 
 //compile scss to css
 function style() {
   //1.where is my scss file
   return gulp.src('./app/assets/styles/styles.css')
   //2.pass the file through scss compiler
-  .pipe(postcss([cssimport, cssvars, nested, autoprefixer]))//.on('error', sass.logError))
+  .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]).on('error', function (errorInfo) {
+    console.log(errorInfo.toString());
+    this.emit('end');
+  }))
   //3.where do i save the compiled css
-  .pipe(gulp.dest('./app/temp/styles'));
+  .pipe(gulp.dest('./app/temp/styles'))
   //4.stream changes to all browser
-  //.pipe(browserSync.stream());
+  .pipe(browserSync.stream());
 }
 
 function watch() {
-  // browserSync.init({
-  //   server: {
-  //     baseDir: './'
-  //   }
-  // })
-  gulp.watch('./app/assets/styles/styles.css', style);
-  //gulp.watch('./*.html').on('change', browserSync.reload);
-  //gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+  
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: './app'
+    }
+  })
+  gulp.watch('./app/assets/styles/**/*.css', style);
+  gulp.watch('./app/*.html').on('change', browserSync.reload);
+  gulp.watch('./app/js/**/*.js').on('change', browserSync.reload);
 }
 
 exports.style = style;
