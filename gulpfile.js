5edb189def5a959c8e8defed4fcdd10a71df3d4a
arpm00 <arpm00@gmail.com>
2019-10-06 22:23:02 +0400

    Styling the header

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,14 @@ const nested = require('postcss-nested');
 const cssimport = require('postcss-import');
 const browserSync = require('browser-sync').create();
 const mixins = require('postcss-mixins');
+const hexrgba = require('postcss-hexrgba');
 
 //compile scss to css
 function style() {
   //1.where is my scss file
   return gulp.src('./app/assets/styles/styles.css')
   //2.pass the file through scss compiler
-  .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]).on('error', function (errorInfo) {
+  .pipe(postcss([cssimport, mixins, cssvars, nested, hexrgba, autoprefixer]).on('error', function (errorInfo) {
     console.log(errorInfo.toString());
     this.emit('end');
   }))
