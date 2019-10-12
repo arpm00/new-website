30ea9a74f07b27bb9158620b8317744bdf768546
arpm00 <arpm00@gmail.com>
2019-10-12 22:28:08 +0400

    Completed

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,12 @@ const hexrgba = require('postcss-hexrgba');
 const webpack = require('webpack');
 const webpackStream = require('webpack-stream');
 const webpackConfig = require('./webpack.config');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const usemin = require('gulp-usemin');
+const rev = require('gulp-rev');
+const cssnano = require('gulp-cssnano');
+const uglify = require('gulp-uglify-es').default;
 
 //compile scss to css
 function style() {
@@ -40,6 +46,46 @@ function scripts(callback) {
     callback();
 }
 
+function deleteDistFolder() {
+  return del("./dist");
+}
+
+function copyGeneralFiles() {
+  const pathToCopy = [
+    '.app/**/*',
+    '!./app/index.html',
+    '!./app/assets/images/**',
+    '!./app/assets/styles/**',
+    '!./app/assets/scripts/**',
+    '!./app/temp',
+    '!./app/temp/**'
+  ]
+  return gulp.src(pathToCopy)
+  .pipe(gulp.dest("./dist"));
+}
+
+function build() {
+  return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icon','!./app/assets/images/icon/**/*'])
+  .pipe(imagemin({
+    progressive: true,
+    interlays: true,
+    multipass: true
+  }))
+  .pipe(gulp.dest("./dist/assets/images"));
+}
+
+function minified() {
+  return gulp.src("./app/index.html")
+  .pipe(usemin({
+    css: [function(){return rev()}, function(){return cssnano()}],
+    jsAttributes: {
+      async: true
+    },
+    js: [function(){return rev()}, function(){return uglify()}]
+  }))
+  .pipe(gulp.dest("./dist"));
+}
+
 function watch() {
   
   browserSync.init({
@@ -51,8 +97,24 @@ function watch() {
   gulp.watch('./app/assets/styles/**/*.css', style);
   gulp.watch('./app/*.html').on('change', browserSync.reload);
   gulp.watch('./app/assets/scripts/**/*.js', scripts);
+  //gulp.watch(gulp.series(deleteDistFolder, build, minified, copyGeneralFiles));
+}
+
+function previewDist() {
+  
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: './dist'
+    }
+  })
+  gulp.watch('./dist/assets/styles/**/*.css', style);
+  gulp.watch('./dist/*.html').on('change', browserSync.reload);
+  gulp.watch('./dist/assets/scripts/**/*.js', scripts);
 }
 
 exports.style = style;
 exports.scripts = scripts;
+exports.build = gulp.series(deleteDistFolder, build, minified, copyGeneralFiles);
 exports.watch = watch;
+exports.dist = previewDist;
